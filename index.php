ed1335f10eb834ed57c2d51424ebd67b29eb4468
Grechelle Uy <grechelleuy@yahoo.com>
2025-03-04 14:07:40 -0500

    Added http template

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('vendor/autoload.php');
+use GuzzleHttp\Client;
+
+
+// TODO: Implement your HTTP client here. 
+
+// TODO: Process the response and build the HTML table here. 
