08af53947a8e44bcb69e77c87d8e62ba81d38083
Bridjette Nania Centro <bridjette_centro@yahoo.com>
2025-03-04 15:09:58 -0500

    Revert "Added http template"
    This reverts commit ed1335f10eb834ed57c2d51424ebd67b29eb4468.

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('vendor/autoload.php');
-use GuzzleHttp\Client;
-
-
-// TODO: Implement your HTTP client here. 
-
-// TODO: Process the response and build the HTML table here. 
